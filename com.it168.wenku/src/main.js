28188792adb044d40bfa824693b96f707ca95f07
gaoyuan <gaoyuan@qq.com>
2019-10-16 20:00:08 +0800

    gaoyuan

--- com.it168.wenku/src/main.js
+++ com.it168.wenku/src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//引入axios
+import axios  from "axios";
+axios.defaults.baseURL = "http://192.168.86.32:9968/api/v1/";
+Vue.prototype.$http=axios;
 // 引入element-ui
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
