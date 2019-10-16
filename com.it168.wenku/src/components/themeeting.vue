15dc9eeb5dfb03868f63ddc11d8cd44df7b10ab0
LiuMin <2764509330@qq.com>
2019-10-16 10:16:07 +0800

    ppt图片部分

---
+++ com.it168.wenku/src/components/themeeting.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="w1200 clearfix">
+      <div class="l2">
+        <!-- 图片 -->
+        <div>
+          <img
+            src="http://pic.wenku.it168.com/positionimgs/bfff592468398c10f9ec35d0ce3babe8.png"
+            alt="第十届中国系统架构师大会（SACC）演讲PPT"
+          />
+        </div>
+        <!-- 筛选项 -->
+        <div class="tit">筛选项</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import "../assets/css/themeeting.css";
+@import "../assets/css/reset.css";
+</style>
