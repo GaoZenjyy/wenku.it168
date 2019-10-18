b842cbdce822eb73d1578f372ba0adfbfea67d47
qttppy <1240294932@qq.com>
2019-10-18 20:14:16 +0800

    gy

--- com.it168.wenku/src/components/login.vue
+++ com.it168.wenku/src/components/login.vue
@@ -102,7 +102,7 @@ export default {
 	            }
 	        }, trigger: 'blur'}],
         password: [
-          {required: true,message: '请输入密码',trigger: "blur" },
+          {required: true,message: '只能输入6-16个字母、数字、下划线必须以字母开头',trigger: "blur" },
           { min: 6, max: 16, message: "长度在 6 到 16个字符" },
           {
             pattern:  /^[A-Za-z0-9~!@#$%\^&\*\(\)_\+\[\]\\{\}\|;':",\.\/<>\?]{6,16}$/,
