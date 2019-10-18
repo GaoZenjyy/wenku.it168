d60d21f00881ba633f111e1b0c075a5a6fc52539
LiuMin <2764509330@qq.com>
2019-10-18 09:02:19 +0800

    渲染最新会议记录

--- com.it168.wenku/src/components/themeeting.vue
+++ com.it168.wenku/src/components/themeeting.vue
@@ -58,16 +58,17 @@
             <el-table-column prop="meeting_title" width="350" label="会议大全"></el-table-column>
             <el-table-column prop="meeting_fileNumber" width="150" label="资料数量"></el-table-column>
             <el-table-column prop="meeting_place" label="会议地点" width="150"></el-table-column>
-            <el-table-column prop="meeting_data" label="浏览量"></el-table-column>
+            <el-table-column prop="meeting_browse" label="浏览量"></el-table-column>
+            <el-table-column prop="meeting_data" label="会议时间"></el-table-column>
           </el-table>
           <!-- 分页 -->
           <div class="block">
             <el-pagination
               @size-change="handleSizeChange"
               @current-change="handleCurrentChange"
-              :current-page="queryInfo.pagenum"
+              :current-page="queryInfo.page"
               :page-sizes="[2, 5, 10, 15]"
-              :page-size="queryInfo.pagesize"
+              :page-size="queryInfo.per_page"
               layout="total, sizes, prev, pager, next, jumper"
               :total="total"
             ></el-pagination>
@@ -97,36 +98,9 @@
         </div>
         <div class="padd">
           <div class="title11">最新收录会议</div>
-          <ul class="huiyi_list">
+          <ul class="huiyi_list" v-for="item in NewMeetingList" :key="item.id">
             <li>
-              <a href>第十届中国数据库技术大会（DTCC）演讲PPT</a>
-            </li>
-            <li>
-              <a href>第九届中国数据库技术大会（DTCC）演讲集锦</a>
-            </li>
-            <li>
-              <a href>后直播时代技术</a>
-            </li>
-            <li>
-              <a href>2017中国大数据技术大会</a>
-            </li>
-            <li>
-              <a href>SDCC 2017互联网应用架构实战峰会</a>
-            </li>
-            <li>
-              <a href>2017 全球云计算开源峰会</a>
-            </li>
-            <li>
-              <a href>PostgreSQL 2017中国技术大会</a>
-            </li>
-            <li>
-              <a href>ECCS-2017企业云计算峰会</a>
-            </li>
-            <li>
-              <a href>2017先知白帽大会（阿里云云盾先知）</a>
-            </li>
-            <li>
-              <a href>第九届中国Hadoop技术峰会2017 北京站</a>
+              <a href >{{item.meeting_title}}}</a>
             </li>
           </ul>
         </div>
@@ -142,15 +116,17 @@ export default {
       classifyselectd: [],
       // 分类列表
       classifyList: [],
+      // 会议记录列表
       meetingList: [],
-
+      // 最新记录列表
+      NewMeetingList: [],
       classifyProps: {
         lable: "attribute_name",
         value: "id"
       },
       queryInfo: {
         page: 1,
-        per_page: 3
+        per_page: 2
       },
       // 总条数
       total: 0
@@ -188,11 +164,18 @@ export default {
     handleCurrentChange(newpagenum) {
       this.queryInfo.page = newpagenum;
       this.getmeetingList();
+    },
+    // 获取最新记录会议
+    async getNewmeetinglist() {
+      const { data: res } = await this.$http.get("included/meeting");
+      console.log(res);
+      this.NewMeetingList = res.data;
     }
   },
   created() {
     this.getcontentList();
     this.getmeetingList();
+    this.getNewmeetinglist();
   }
 };
 </script>
